52280888eeb48cb06ccf9c86ede91863ab00ca29
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-09-29 09:15:25 +0530

    opt

--- src/app/(profile)/profile/page.tsx
+++ src/app/(profile)/profile/page.tsx
@@ -14,29 +14,46 @@ export default function profile() {
   const resumeAnalyse = useResumeAnalyse();
   return (
     <div className="m-20">
-      <div className="flex justify-center items-center ">
+      <div className="flex justify-center items-center mb-6">
         <Profile />
       </div>
-      <div>
-        <button onClick={enterRole.onOpen}>
-          click to open profile builder
+
+      <div className="flex justify-between items-center">
+        <button
+          onClick={enterRole.onOpen}
+          className="bg-gradient-to-r from-blue-400 to-blue-600 hover:from-blue-500 hover:to-blue-700 text-white font-bold py-2 px-6 rounded"
+        >
+          Profile Builder
         </button>
-      </div>
-      <div>
+
         {recruiter.isRecruiter && (
-          <button onClick={addjob.onOpen}>click to add job</button>
+          <button
+            onClick={addjob.onOpen}
+            className="bg-gradient-to-r from-blue-400 to-blue-600 hover:from-blue-500 hover:to-blue-700 text-white font-bold py-2 px-6 rounded"
+          >
+            Add Job
+          </button>
         )}
-      </div>
-      <div>
+
         {!recruiter.isRecruiter && (
-          <button onClick={resumeBuild.onOpen}>click to build resume</button>
-        )}
-      </div>
-      <div>
-        {!recruiter.isRecruiter && (
-          <button onClick={resumeAnalyse.onOpen}>click to analyse resume</button>
+          <>
+            <button
+              onClick={resumeBuild.onOpen}
+              className="bg-gradient-to-r from-blue-400 to-blue-600 hover:from-blue-500 hover:to-blue-700 text-white font-bold py-2 px-6 rounded"
+            >
+              Resume Builder
+            </button>
+
+            <button
+              onClick={resumeAnalyse.onOpen}
+              className="bg-gradient-to-r from-blue-400 to-blue-600 hover:from-blue-500 hover:to-blue-700 text-white font-bold py-2 px-6 rounded"
+            >
+              Resume Analyzer
+            </button>
+          </>
         )}
       </div>
     </div>
+
   );
 }
